c73ab3e323e79e5b7fd8e9436cc7ce3dac28ce4f
Andrey <Weroset12@gmail.com>
2023-12-13 20:59:07 +0300

    Fixed

--- captcha_resolver/yolov8/utils.py
+++ captcha_resolver/yolov8/utils.py
@@ -94,23 +94,8 @@ def draw_detections(image, boxes, scores, class_ids, mask_alpha=0.3):
 
         label = class_names[class_id]
         caption = f'{label} {int(score * 100)}%'
-        draw_text(det_img, caption, box, color, font_size, text_thickness)
-
     return det_img
 
-
-
-def draw_text(image: np.ndarray, text: str, box: np.ndarray, color: tuple[int, int, int] = (0, 0, 255),
-              font_size: float = 0.001, text_thickness: int = 2) -> np.ndarray:
-    x1, y1, x2, y2 = box.astype(int)
-    (tw, th), _ = cv2.getTextSize(text=text, fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                                  fontScale=font_size, thickness=text_thickness)
-    th = int(th * 1.2)
-
-    cv2.rectangle(image, (x1, y1),
-                  (x1 + tw, y1 - th), color, -1)
-
-    return cv2.putText(image, text, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), text_thickness, cv2.LINE_AA)
 
 def draw_masks(image: np.ndarray, boxes: np.ndarray, classes: np.ndarray, mask_alpha: float = 0.3) -> np.ndarray:
     mask_img = image.copy()
