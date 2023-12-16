6e0b3af24bb86bb5da50aa1cbfe4a9bc884cdad3
Andrey <Weroset12@gmail.com>
2023-12-16 23:19:07 +0300

    Docker-compose updated

--- captcha_resolver/yolov8/YOLOv8.py
+++ captcha_resolver/yolov8/YOLOv8.py
@@ -20,7 +20,8 @@ class YOLOv8:
 
     def initialize_model(self, path):
         self.session = onnxruntime.InferenceSession(
-            path, providers=onnxruntime.get_available_providers()
+            path, providers=['CUDAExecutionProvider', 'CPUExecutionProvider']
+
         )
         # Get model info
         self.get_input_details()
